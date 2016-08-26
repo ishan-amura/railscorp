4485e2bef584bba090c373ac5d3072e3d7493165
Ishan Kanade <ishan@amuratech.com>
2016-08-26 14:41:23 +0530

    Performed Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826071332) do
+ActiveRecord::Schema.define(version: 20160826090713) do
 
   create_table "addresses", force: :cascade do |t|
-    t.text     "location"
     t.integer  "resource_id"
     t.string   "resource_type"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "city"
+    t.string   "state"
+    t.text     "locality"
   end
 
   add_index "addresses", ["resource_type", "resource_id"], name: "index_addresses_on_resource_type_and_resource_id"
